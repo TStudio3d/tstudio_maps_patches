cfc35b8e03ee1d7a68781033f89ab508d05c04b9
zShxdow <shxdowyt@gmail.com>
2025-11-18 22:54:20 +0100

    add compatibility patches for Rags & Riches

---
+++ tstudio_zpatch_mrpark_amb_rr/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'tstudio_zpatch_mrpark_amb_rr'
+author 'TStudio'
+description 'Fix for Mission Row Park & Ambitioneers Rags&Riches'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+dependency '/assetpacks'
