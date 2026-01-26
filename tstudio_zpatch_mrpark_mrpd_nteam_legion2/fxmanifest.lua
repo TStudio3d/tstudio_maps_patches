3a6e2c4be3c39e8f1352e468d1b4f58f61fbb599
Saif Al-Dilaimi <saif.aldilaimi@gmail.com>
2026-01-26 12:18:34 +0100

    Add compatibility patches for NTeam Legion V2 and Molo Legion Square Memorial

---
+++ tstudio_zpatch_mrpark_mrpd_nteam_legion2/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'tstudio_zpatch_mrpark_mrpd_nteam_legion2'
+author 'TStudio'
+description 'Fix for Mission Row Park & MRPD & NTeam Legion V2'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+dependency '/assetpacks'
---
+++ tstudio_zpatch_mrpd_th_nteam_legion2/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'tstudio_zpatch_mrpd_th_nteam_legion2'
+author 'TStudio'
+description 'Fix for MRPD & Tropical Heights & NTeam Legion V2'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+dependency '/assetpacks'
