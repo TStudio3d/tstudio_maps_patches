5040617a3d6e879c0be85053887b3e73516011eb
Saif Al-Dilaimi <saif.aldilaimi@gmail.com>
2026-02-10 22:44:32 +0100

    Refactor peak towers patches - rename and reorganize folders

--- tstudio_zpatch_peak_towers_legionsquare_garage/fxmanifest.lua
+++
@@ -1,7 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-dependencies {
-    'tstudio_legionsquare_garage',
-    'tstudio_peak_towers',
-}
--- tstudio_zpatch_peak_towers_tropical_heights/fxmanifest.lua
+++
@@ -1,7 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-dependencies {
-    'tstudio_peak_towers',
-    'tstudio_tropical_heights',
-}
