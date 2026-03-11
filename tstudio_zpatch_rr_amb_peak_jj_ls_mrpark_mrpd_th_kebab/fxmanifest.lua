994c18b8a6620bf9dfa5ac8e67b107edbad1268e
IlMelons <melonss2002@gmail.com>
2026-03-11 13:39:40 +0100

    Patch All TStudio Legion Maps & Amb MHC (1.5.5)

---
+++ tstudio_zpatch_rr_amb_peak_jj_ls_mrpark_mrpd_th_kebab/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'tstudio_zpatch_rr_amb_peak_jj_ls_mrpark_mrpd_th_kebab'
+description 'Fix for All TStudio Legion Maps & Ambitioneers Rags&Riches (1.5.5)'
+author 'TStudio'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/*.*',
+}
