1ae122d50775326358a3224436e3940c7f825a60
IlMelons <melonss2002@gmail.com>
2026-04-15 13:31:25 +0200

    Fix for Peak & Legion & Jurassic & Ajaxon Aldore

---
+++ tstudio_zpatch_peak_ls_jj_ajaxon_aldore/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+name 'tstudio_zpatch_peak_ls_jj_ajaxon_aldore'
+author 'TStudio'
+description 'Fix for Peak Towers & Legion Square & Jurassic Jackpot & Ajaxon Aldore Hospital'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+  'stream/*.*',
+}
