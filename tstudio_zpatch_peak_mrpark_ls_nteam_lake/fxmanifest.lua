9e2e0e50a0f32b5384c25217bff6c8e1cb686e23
IlMelons <melonss2002@gmail.com>
2026-06-04 23:19:09 +0200

    Add New patches for NTeam Lake

---
+++ tstudio_zpatch_peak_mrpark_ls_nteam_lake/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'tstudio_zpatch_peak_mrpark_ls_nteam_lake'
+author 'TStudio'
+description 'Fix for Peak Towers & Legion Square & Mission Row Park & NTeam Lake'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/*.*',
+}
