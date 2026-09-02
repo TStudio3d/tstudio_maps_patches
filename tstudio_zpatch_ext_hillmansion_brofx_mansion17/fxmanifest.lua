908fb2a6aaf366351dcad8c38004c049c0285b17
IlMelons <melonss2002@gmail.com>
2026-09-02 17:41:57 +0200

    Add Crime Exteriors patches 5/6

---
+++ tstudio_zpatch_ext_hillmansion_brofx_mansion17/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'tstudio_zpatch_ext_hillmansion_brofx_mansion17'
+description 'Fix for Hill Mansion & brofx Mansion 17'
+author 'TStudio'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+dependency '/assetpacks'
---
+++ tstudio_zpatch_ext_japanesemansion_brofx_mansion14/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'tstudio_zpatch_ext_japanesemansion_brofx_mansion14'
+description 'Fix for Japanese Mansion & brofx Mansion 14'
+author 'TStudio'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+dependency '/assetpacks'
