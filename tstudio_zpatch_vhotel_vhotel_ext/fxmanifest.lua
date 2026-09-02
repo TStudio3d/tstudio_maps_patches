89d431c94c50f2503710abad8e11de695dc06b11
IlMelons <melonss2002@gmail.com>
2026-09-02 08:25:54 +0200

    Add VHotel patch

---
+++ tstudio_zpatch_vhotel_vhotel_ext/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'tstudio_zpatch_vhotel_vhotel_ext'
+author 'TStudio'
+description 'Fix for VHotel & VHotel Exterior'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+dependency '/assetpacks'
